7211fb26f16375d0508a7d41c6d087122613e64a
Ajewole Emmanuel <ajewoletemitope277@gmail.com>
2026-07-04 02:43:22 +0100

    Add Student management system

---
+++ STUDENT_MANAGEMENT_SYSTEM.py
@@ -0,0 +1,89 @@
+class Student:
+    def __init__(self, name, age, department, matric_no):
+        self.name = name
+        self.age = age 
+        self.department = department
+        self.matric_no = matric_no
+    
+    def introduce(self):
+        print(
+            f'My name is {self.name}. '
+            f'I am studying {self.department}'
+        )
+    
+    def display_info(self):
+        print("Inside display_info")
+        print(f'Name: {self.name}')
+        print(f'Age: {self.age}')
+        print(f'Department: {self.department}')
+        print(f'Matric Number: {self.matric_no}')
+
+
+def menu():
+    print("===== Student Management System ===== " \
+    "\n 1. Add student " \
+    "\n 2. View Student " \
+    "\n 3. Search student " \
+    "\n 4. Update student" \
+    " \n 5. Delete student " \
+    "\n 6. Save students " \
+    "\n 7. Exit")
+
+    choice = int(input("Enter your choice: "))
+    return choice
+
+students = []
+
+while True:
+    
+    saveChoice = menu()
+    if saveChoice == 1:
+        name = input("Enter student's name: ")
+        age = input("Enter age: ")
+        dept = input("Enter department: ")
+        mat = input("Enter matric number: ")
+        
+        new_student = Student(name, age, dept, mat)
+        students.append(new_student)
+        print("Student added successfully!")
+
+    elif saveChoice == 2:
+        if not students:
+            print("No students available.")
+        else:
+            for s in students:
+                s.display_info()
+                
+    elif saveChoice == 3:
+        getName = input("Enter student's name: ")
+        found = False
+        for s in students:
+            if getName.lower() == s.name.lower():
+                s.display_info()
+                found = True
+                break
+        if not found:
+            print("Student not found.")
+    
+    elif saveChoice == 4:
+        getName = input("Enter student's name: ")
+        for s in students:
+            if getName.lower() == s.name.lower():
+                new_name = input("New name: ")
+                new_age = input("New age: ")
+                new_department = input("New department: ")
+                new_matric = input("New matric number: ")
+
+                s.name = new_name
+                s.age = new_age
+                s.department = new_department
+                s.matric_no = new_matric
+
+                print("Student updated successfully!")
+                break
+        else:
+            print("Student not found.")
+    elif saveChoice == 7:
+        print("Goodbye!")
+        break
+
