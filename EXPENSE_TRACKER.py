fdad600cca6cab2c2bf89d955e0586dcbd3d79aa
Ajewole Emmanuel <ajewoletemitope277@gmail.com>
2026-08-13 05:06:41 +0100

    Creating the list variable and menu function

--- EXPENSE_TRACKER.py
+++ EXPENSE_TRACKER.py
@@ -10,3 +10,17 @@ class Expense:
 
 expense = Expense("Cereals", 2500, "Chinese-rice", "2023-06-01")
 print(expense)
+
+expenses = []
+
+def menu():
+    print("WELCOME TO EXPENSE TRACKER" \
+          "/n 1. Add Expense" \
+          "/n 2. View Expenses" \
+          "/n 3. Search Expense" \
+          "/n 4. Delete Expense" \
+          "/n 5. Show total spending" \
+          "/n 6. Save Expenses" \
+          "/n 7. Exit"
+          )
+    
