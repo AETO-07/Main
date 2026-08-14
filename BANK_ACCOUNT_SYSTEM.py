50ed009d62691cbea18d36587d8f81e8b330f383
Ajewole Emmanuel <ajewoletemitope277@gmail.com>
2026-08-14 08:52:18 +0100

    Creating the menu and structuring the account list and while loop

--- BANK_ACCOUNT_SYSTEM.py
+++ BANK_ACCOUNT_SYSTEM.py
@@ -10,5 +10,26 @@ class BankAccount:
         return (
             f"Account Name: {self.account_name} | "
             f"Account_No: {self.account_number} | "
-            f"Balance: {self.balance:,.2f}"
+            f"Balance: ₦{self.balance:,.2f}"
         )
+
+accounts = []
+
+def menu():
+    print(
+        "WELCOME TO BANK ACCOUNT SYSTEM" \
+        "\n 1. Create Account" \
+        "\n 2. View Accounts" \
+        "\n 3. Deposit" \
+        "\n 4. Withdraw" \
+        "\n 5. Transfer" \
+        "\n 6. View Transaction History" \
+        "\n 7. Save Accounts" 
+        "\n 8. Exit"
+    )
+
+    ask_choice = int(input("Enter your choice: "))
+    return ask_choice
+
+while True:
+    save_choice = menu()
